e8b8bd33228f69cf1581f4c133cd7ed3b203a896
Leo Dube <leo.s.dube@gmail.com>
2022-08-11 12:10:25 -0400

    Update gatsby config files

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,6 +1,13 @@
 import type { GatsbyConfig } from "gatsby";
 import "dotenv/config";
 
+const strapiConfig = {
+  apiURL: process.env.STRAPI_API_URL,
+  accessToken: process.env.STRAPI_TOKEN,
+  collectionTypes: ["article", "company", "author"],
+  singleTypes: [],
+};
+
 const config: GatsbyConfig = {
   siteMetadata: {
     title: "Leo Dube",
@@ -8,19 +15,15 @@ const config: GatsbyConfig = {
     author: "@leodube",
     siteUrl: `https://www.leodube.ca`,
   },
-
+  
   proxy: {
     prefix: "/admin",
     url: process.env.ADMIN_API_URL || "http://localhost:1337",
   },
   plugins: [
     {
-      resolve: "gatsby-source-graphql",
-      options: {
-        typeName: "Keystone",
-        fieldName: "keystone",
-        url: process.env.ADMIN_API_URL,
-      },
+      resolve: "gatsby-source-strapi",
+      options: strapiConfig,
     },
     {
       resolve: "@chakra-ui/gatsby-plugin",
