0070ae612ce6ed7670d1e91323a805c62039c97b
Leo Dube <leo.s.dube@gmail.com>
2022-09-08 12:26:28 -0700

    Remove keystone

--- backend/src/models/index.ts
+++
@@ -1,9 +0,0 @@
-import { user } from "./user";
-import { page } from "./page";
-import { job } from "./job";
-import { project } from "./project";
-import { skill } from "./skill";
-import { image } from "./image";
-import { seo } from "./seo";
-
-export { user, page, job, project, skill, image, seo };
